00c44e802fe98f9369080e29f4026c713b7cafa1
Dreamstrue <a332576017@163.com>
2016-10-04 16:30:35 +0800

    20160930

--- src/com/price/LoginController.java
+++ src/com/price/LoginController.java
@@ -5,11 +5,6 @@ import com.jfinal.core.Controller;
 public class LoginController extends Controller{
 
 	public void index(){
-//		render("/view/login.jsp");
-		render("/index.html");
-	}
-	
-	public void main(){
-		render("/view/main.jsp");
+		render("/index.jsp");
 	}
 }
